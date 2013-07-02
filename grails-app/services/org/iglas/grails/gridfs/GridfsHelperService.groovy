26f93b2b79a8df067795ae6eb1a6eea82c4e5f01
Paul Osborne <hello@paulosborne.me.uk>
2013-07-02 20:26:44 +0100

    Uncomment code. (still needs to be tested)

--- grails-app/services/org/iglas/grails/gridfs/GridfsHelperService.groovy
+++ grails-app/services/org/iglas/grails/gridfs/GridfsHelperService.groovy
@@ -11,28 +11,28 @@ import com.mongodb.gridfs.GridFSDBFile
 
 class GridfsHelperService {
 	
-//	private GridFS getStore() {
-//		def config = new UserConfig(GridfsService.configName).get()
-//		GridFS gfsFiles
-//		try {
-//			Mongo mongo = new Mongo(config.db.host)
-//			DB db  = mongo.getDB(config.db.name)
-//			gfsFiles = new GridFS(db, config.db.collection )
-//		}catch (Exception e){
-//			println(e.message)
-//			def errorMsg = "Connection to database error: ${e.message}."
-//			throw new GrailsTagException(errorMsg)
-//		}
-//		gfsFiles
-//	}
+	private GridFS getStore() {
+		def config = new UserConfig(GridfsService.configName).get()
+		GridFS gfsFiles
+		try {
+			Mongo mongo = new Mongo(config.db.host)
+			DB db  = mongo.getDB(config.db.name)
+			gfsFiles = new GridFS(db, config.db.collection )
+		}catch (Exception e){
+			println(e.message)
+			def errorMsg = "Connection to database error: ${e.message}."
+			throw new GrailsTagException(errorMsg)
+		}
+		gfsFiles
+	}
 
 	
 	GridFSDBFile findOne(ObjectId oid) { 
-//		store.findOne(oid)
+		store.findOne(oid)
 	}
 	
 	GridFSDBFile findOne(String filename) { 
-//		store.findOne(filename)
+		store.findOne(filename)
 	}
 
 }
